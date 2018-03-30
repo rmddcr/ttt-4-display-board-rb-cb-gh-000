979257f4095883a0e227b67ea5ff33760a98f005
Damitha Ranasingha <rmddcr@gmail.com>
2018-03-30 10:55:58 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,18 @@
 # Define display_board that accepts a board and prints
+def display_board(input_array)
+
+  puts " #{input_array[0]} | #{input_array[2]} | #{input_array[3]} "
+  puts "-----------"
+  puts " #{input_array[4]} | #{input_array[5]} | #{input_array[6]} "
+  puts "-----------"
+  puts " #{input_array[7]} | #{input_array[8]} | #{input_array[9]} "
+  
+
+end
 # out the current state.
+#    |   |   
+# -----------
+#    | X |   
+# -----------
+#    |   |   
+```
